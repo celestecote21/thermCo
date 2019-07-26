e32449a406a71f7268014288c694fb37f9d54070
celeste <celeste@cote21.fr>
2019-07-26 03:22:46 +0200

    Initial commit

---
+++ app/src/main/java/com/celeste/thermco/defineT.kt
@@ -0,0 +1,12 @@
+package com.celeste.thermco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class defineT : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_define_t)
+    }
+}
